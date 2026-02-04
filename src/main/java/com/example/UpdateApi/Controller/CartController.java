9668119ff15ed7bcf9988d0f3379d6af92f6999b
Hemanth234565 <hemanthappakondu23@gmail.com>
2026-02-04 19:23:07 +0530

    changes need to done

--- src/main/java/com/example/UpdateApi/Controller/CartController.java
+++ src/main/java/com/example/UpdateApi/Controller/CartController.java
@@ -2,16 +2,20 @@ package com.example.UpdateApi.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.UpdateApi.Model.Cart;
-import com.example.UpdateApi.Service.CartService;
+import com.example.UpdateApi.Service.CartGrpcClientService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CartController {
-
+/* 
     @Autowired
     private CartService cartService;
 
@@ -20,5 +24,20 @@ public class CartController {
         cartService.updateproducts(cart);
 
         return ResponseEntity.ok("Product updated successfully");
+    }  */
+
+
+    private CartGrpcClientService grpcClientService;
+
+    @Autowired
+    public void UpdateController(CartGrpcClientService grpcClientService) {
+        this.grpcClientService = grpcClientService;
     }
+
+    @PostMapping("/update/cart")
+    public String updateCart() {
+        return grpcClientService.updateCart(1, "Hemanth", 25);
+    }
+
+
 }
